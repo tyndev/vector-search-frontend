f88f2ec15c716526a69463c87d8c691ca064d528
tyndev <17089219+tyndev@users.noreply.github.com>
2024-05-08 07:07:16 -0700

    fixed displaying json

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
       <section className="py-6 md:py-12">
         <div className="container px-4 md:px-6">
           <h1 className="font-bold sm:text-4xl md:text-3xl">Hello Vector Search</h1>
-          <h2> {data} </h2>
+          <h2> {data.Hello} </h2>
         </div>
       </section>
 
